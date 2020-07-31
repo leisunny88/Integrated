1da9523f37a306e24ef59287838559d593e885bc
leisunny88 <57985766+leisunny88@users.noreply.github.com>
2020-07-31 17:05:38 +0800

    model

--- spiders/models.py
+++ spiders/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class MeiZiTu(models.Model):
+    id = models.AutoField(primary_key=True)
     tutime = models.CharField(max_length=30)
     imgname = models.CharField(max_length=100)
     link = models.CharField(max_length=100)
@@ -10,18 +11,47 @@ class MeiZiTu(models.Model):
 
 
 class NewsDataModel(models.Model):
+    id = models.AutoField(primary_key=True)
     newstime = models.CharField(max_length=30)
     newurl = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
+    detail = models.OneToOneField('NewsDataText', on_delete=models.CASCADE)
+
+
+class NewsDataText(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+
+    class Meta:
+        db_table = "news_text"
 
 
 class ANovelModel(models.Model):
+    id = models.AutoField(primary_key=True)
     antime = models.CharField(max_length=30)
-    name = models.CharField(max_length=100, null=None)
-    link = models.CharField(max_length=100, null=None)
+    name = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+    detail = models.OneToOneField('ANovelDataText', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "anovel"
+
+
+class ANovelDataText(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+
+    class Meta:
+        db_table = "anovel_text"
 
 
 class VideoModel(models.Model):
-    playtime = models.CharField(max_length=30, null=None)
-    title = models.CharField(max_length=100, null=None)
-    link = models.CharField(max_length=100, null=None)
+    id = models.AutoField(primary_key=True)
+    playtime = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
+    link = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "video"

216398fd48ee3d762a152224acd3045b2bd89b72
leisunny88 <57985766+leisunny88@users.noreply.github.com>
2020-08-03 19:00:56 +0800

    update

--- spiders/views.py
+++ spiders/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from spiders import models
-from spiders.news import NewData
+from spiders.news import NewData, Video
+import threading
 
 
 def add_news(request):
@@ -18,8 +19,19 @@ def add_news(request):
             newurl=data[1],
             title=data[2]
         )
-
-
+    for data_list in text_data_list:
+        models.NewsDataText.objects.create(
+            name=data_list[0],
+            text=data_list[1]
+        )
+    vd = Video()
+    video_list = vd.get_video_link()
+    for video_data in video_list:
+        models.VideoModel.objects.create(
+            playtime=video_data[0],
+            title=video_data[1],
+            link=video_data[2]
+        )
     response = HttpResponse("添加成功")
     return response
 
